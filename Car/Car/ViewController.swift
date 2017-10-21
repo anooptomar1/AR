348fdb28747e1cb983adfec9ef84ed8952079c44
zhangsihuai <727087311@qq.com>
2017-10-21 21:44:09 +0800

    玩具车

--- Car/Car/ViewController.swift
+++ Car/Car/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 import ARKit
+import CoreMotion
 
 class ViewController: UIViewController {
     
     let configuration = ARWorldTrackingConfiguration()
+    //运动传感器
+    let motionManager = CMMotionManager()
+    
+    var accelerationValues: [Double] = [0, 0]
     
     var vehicle = SCNPhysicsVehicle()
     
@@ -23,6 +28,8 @@ class ViewController: UIViewController {
         scnView.session.run(configuration)
         scnView.delegate = self
         scnView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        
+        setupMotionManager()
     }
     
     @IBAction func addButtonClick(_ sender: UIButton) {
@@ -45,6 +52,7 @@ class ViewController: UIViewController {
         }
         
         let carBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: carNode, options: [SCNPhysicsShape.Option.keepAsCompound : true]))
+        carNode.physicsBody = carBody
         
         self.vehicle = SCNPhysicsVehicle(chassisBody: carBody, wheels: wheels)
         self.scnView.scene.physicsWorld.addBehavior(vehicle)
@@ -54,12 +62,13 @@ class ViewController: UIViewController {
         
     }
     
-    func createPlane(by anchor: ARPlaneAnchor) -> SCNNode {
+    func createPlane(for anchor: ARPlaneAnchor) -> SCNNode {
         let plane = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
         plane.firstMaterial?.diffuse.contents = UIImage(named: "concrete")
         plane.firstMaterial?.isDoubleSided = true
         let planeNode = SCNNode(geometry: plane)
         planeNode.position = SCNVector3(anchor.center.x, anchor.center.y, anchor.center.z)
+        planeNode.eulerAngles = SCNVector3(.pi * 0.5, 0, 0)
         //创建物理效果
         //static: 不受重力影响，但是会与其它的物体碰撞
         let staticBody = SCNPhysicsBody.static()
@@ -68,11 +77,44 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController {
+    func setupMotionManager() {
+        if motionManager.isAccelerometerAvailable {
+            //1秒钟更新60次
+            motionManager.accelerometerUpdateInterval = 1/60
+            motionManager.startAccelerometerUpdates(to: .main) { data, error in
+                if let error = error {
+                    print(error.localizedDescription)
+                    return
+                }
+//                print(data!.acceleration.x, data!.acceleration.y)
+//                self.accelerometerDidChange(acceleration: data!.acceleration)
+            }
+            
+        }else {
+            //没有传感器
+            fatalError()
+        }
+    }
+    
+    func accelerometerDidChange(acceleration: CMAcceleration) {
+        accelerationValues[0] = update(current: accelerationValues[0], update: acceleration.x)
+        accelerationValues[1] = update(current: accelerationValues[1], update: acceleration.y)
+    }
+    
+    func update(current: Double, update: Double) -> Double {
+        let fator: Double = 0.5
+        return update + 0.5 + current * (1 - fator)
+    }
+}
+
+
 extension ViewController: ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         if let planeAnchor = anchor as? ARPlaneAnchor {
-            let planeNode = createPlane(by: planeAnchor)
+            let planeNode = createPlane(for: planeAnchor)
             node.addChildNode(planeNode)
+            print("didAdd node for anchor")
         }
     }
     
@@ -81,8 +123,9 @@ extension ViewController: ARSCNViewDelegate {
             node.enumerateChildNodes({ (node, _) in
                 node.removeFromParentNode()
             })
-            let planeNode = createPlane(by: planeAnchor)
+            let planeNode = createPlane(for: planeAnchor)
             node.addChildNode(planeNode)
+            print("didUpdate node for anchor")
         }
     }
     
